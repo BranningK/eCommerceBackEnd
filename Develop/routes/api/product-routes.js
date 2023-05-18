4e1e0e86b4c298320b93f6f10f03142acddae217
Branning <bmk2110@gmail.com>
2023-05-18 15:29:05 -0400

    finished still need to fix readme

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -101,17 +101,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  Product.destroy({
-    where: {
-      product_id: req.params.product_id,
-    },
-  })
-    .then((deletedProduct) => {
-      res.json(deletedProduct);
-    })
-    .catch((err) => res.json(err));
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!product) {
+      res.status(404).json({ message: "There is no product with that id" });
+      return;
+    }
+    res.status(200).json("Product Deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
